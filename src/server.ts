d65adc97a2f8cac7adc47fc7d2ef141688fff9ee
yamada-michel27 <f.atsuya.personal@gmail.com>
2026-06-30 17:05:31 +0900

    ポート設定の取得方法を明示的に数値に変換

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ const server = createServer((req, res) => {
     res.end(html);
 });
 
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT) || 3000;
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}!`);
